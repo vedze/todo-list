00dde93617bbf9792328ecd2ec7445434c18314e
vedze <ezdev00@gmail.com>
2024-05-16 23:18:23 +0900

    Feat: To-do 추가 기능

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,50 @@
+import { useRef, useState } from "react";
 import "./App.css";
 import Editor from "./components/Editor";
 import Header from "./components/Header";
 import List from "./components/List";
 
+/* 임시 데이터 */
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(), // 현재 시간에 해당하는 타임스탬프
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "JavaScript 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "NextJS 공부하기",
+    date: new Date().getTime(),
+  },
+];
+
 function App() {
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+
+    setTodos([newTodo, ...todos]); // 맨 앞에 새 Todo 추가, 뒤로 붙이면 맨 뒤에 추가됨
+  };
+
   return (
     <div className="App">
       <Header />
-      <Editor />
+      <Editor onCreate={onCreate} />
       <List />
     </div>
   );
