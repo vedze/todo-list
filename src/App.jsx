9e47cdc14b1ae00e62e700119c70ffcf66fcc453
vedze <ezdev00@gmail.com>
2024-05-18 22:54:33 +0900

    Dev: 프로젝트 최적화 (#2)
    * Feat: useReducer를 이용한 간단 카운터
    
    * Fix: 상태관리 useState -> useReducer 변경
    
    * Fix: useMemo를 이용한 리렌더링 방지
    
    analyzing checked data 기능 추가 후 불필요한 리렌더링 방지 위해 useMemo 사용
    
    * Fix: React.memo를 이용한 리렌더링 방지
    
    내장함수  memo를 이용한 Header 컴포넌트 리렌더링 방지와 TodoItem HOC 변환
    
    * Fix: useCallback 최적화

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useRef, useState } from "react";
 import "./App.css";
 import Editor from "./components/Editor";
 import Header from "./components/Header";
 import List from "./components/List";
+import { useRef, useState, useReducer, useCallback } from "react";
 
 /* 임시 데이터 */
 const mockData = [
@@ -26,34 +26,50 @@ const mockData = [
   },
 ];
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) =>
+        item.id === action.targetId ? { ...item, isDone: !item.isDone } : item
+      );
+    case "DELETE":
+      return state.filter((item) => item.id !== action.targetId);
+    default:
+      return state;
+  }
+}
+
 function App() {
-  const [todos, setTodos] = useState(mockData);
+  const [todos, dispatch] = useReducer(reducer, mockData);
   const idRef = useRef(3);
 
-  const onCreate = (content) => {
-    const newTodo = {
-      id: idRef.current++,
-      isDone: false,
-      content: content,
-      date: new Date().getTime(),
-    };
+  const onCreate = useCallback((content) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        date: new Date().getTime(),
+      },
+    });
+  }, []);
 
-    setTodos([newTodo, ...todos]); // 맨 앞에 새 Todo 추가, 뒤로 붙이면 맨 뒤에 추가됨
-  };
+  const onUpdate = useCallback((targetId) => {
+    dispatch({
+      type: "UPDATE",
+      targetId: targetId,
+    });
+  }, []);
 
-  // todos 중 targetId와 일치하는 id를 갖는 todo item만 변경한 새로운 배열 반환
-  const onUpdate = (targetId) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo
-      )
-    );
-  };
-
-  // targetId가 아닌 todo들 filter
-  const onDelete = (targetId) => {
-    setTodos(todos.filter((todo) => todo.id !== targetId));
-  };
+  const onDelete = useCallback((targetId) => {
+    dispatch({
+      type: "DELETE",
+      targetId: targetId,
+    });
+  }, []);
 
   return (
     <div className="App">
